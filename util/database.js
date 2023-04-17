183917be082bfe98be9409e2bde11f9b6026af5c
Ashuballawar <ashutoshballawar@gmail.com>
2023-04-17 12:38:44 +0530

    connected to mongodb

---
+++ util/database.js
@@ -0,0 +1,24 @@
+const mongodb=require('mongodb')
+
+const MongoClient=mongodb.MongoClient;
+let _db;
+const mongoConnect=(callback)=>{
+MongoClient.connect('mongodb+srv://ashutoshballawar:ashutoshballawar@cluster0.cxidpz2.mongodb.net/expencetracker?retryWrites=true&w=majority')
+.then((client)=>{
+    console.log('connected')
+    _db=client.db()
+    callback()
+}).catch((err)=>{
+    console.log(err)
+    throw err
+});
+}
+const getDB=()=>{
+    if(_db){
+        return _db;
+    }
+
+    throw 'No database found!'
+}
+exports.mongoConnect=mongoConnect
+exports.getDB=getDB
